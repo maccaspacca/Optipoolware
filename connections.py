6ddc477cba3ceb32eae040a5790f832bbaece57b
maccaspacca <maccaspacca@virginmedia.com>
2017-09-19 21:17:25 +0100

    Testing 19092017

--- connections.py
+++ connections.py
@@ -1,35 +1,44 @@
-import select, picklemagic, ast
+import select, picklemagic, ast, json
 
 def send(sdef, data, slen):
     sdef.setblocking(0)
 
-    sdef.sendall(str(len(str(picklemagic.safe_dumps(data)))).encode("utf-8").zfill(slen))
-    sdef.sendall(str(picklemagic.safe_dumps(data)).encode("utf-8"))
-    #print(pickle.dumps(data))
+    sdef.sendall(str(len(str(json.dumps(data)))).encode("utf-8").zfill(slen))
+    sdef.sendall(str(json.dumps(data)).encode("utf-8"))
 
 def receive(sdef, slen):
     sdef.setblocking(0)
-    ready = select.select([sdef], [], [], 60)
+    ready = select.select([sdef], [], [], 240)
     if ready[0]:
-        data = int(sdef.recv(slen))  # receive length
-        #print ("To receive: {}".format(data))
+        try:
+            data = int(sdef.recv(slen))  # receive length
+            #print ("To receive: {}".format(data))
+        except:
+            raise RuntimeError("No data received, probably probing") #do away with the invalid literal for int
+
     else:
         raise RuntimeError("Socket timeout")
 
     chunks = []
     bytes_recd = 0
     while bytes_recd < data:
-        ready = select.select([sdef], [], [], 60)
+        ready = select.select([sdef], [], [], 240)
         if ready[0]:
             chunk = sdef.recv(min(data - bytes_recd, 2048))
-            if chunk == b'':
+            if not chunk:
                 raise RuntimeError("Socket connection broken")
             chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
         else:
              raise RuntimeError("Socket timeout")
 
-    segments = b''.join(chunks).decode("utf-8")
     #print ("Received segments: {}".format(segments))
 
-    return picklemagic.safe_loads(ast.literal_eval(segments))
+    try:
+        segments = b''.join(chunks).decode("utf-8")
+        return json.loads(segments)
+
+    except: #compatibility
+        segments = b''.join(chunks).decode("utf-8")
+        return picklemagic.safe_loads(ast.literal_eval(segments))
+
--- optiexplorer.py
+++ optiexplorer.py
@@ -17,6 +17,10 @@ def hello():
 	shares = sqlite3.connect('shares.db')
 	shares.text_factory = str
 	s = shares.cursor()
+	
+	oldies = sqlite3.connect('archive.db')
+	oldies.text_factory = str
+	o = oldies.cursor()
 
 	addresses = []
 	for row in s.execute("SELECT * FROM shares"):
@@ -27,33 +31,59 @@ def hello():
 		if shares_address not in addresses:
 			addresses.append(shares_address)
 
-	output_shares = []
+	total_hash = 0
+	output_shares = []	
 	output_timestamps = []
 	view = []
 
+	view.append('<head class="navbar navbar-inverse navbar-fixed-top" role="banner">\n')
+	view.append(' <meta charset="utf-8">')
+	view.append('   <meta http-equiv="X-UA-Compatible" content="IE=edge">')
+	view.append('   <meta name="viewport" content="width=device-width, initial-scale=1.0">')
+	view.append('  <link rel="shortcut icon" href="favicon.ico">')
+	view.append('  <title>pool.bismuth.online</title>')
+	view.append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >')
+	view.append('  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" >')
+	view.append('<div class="container">')
+	view.append('<a class="navbar-brand" href="#">pool.bismuth.online <span id="currency"></span></a>')
+	view.append('</div>')
+	view.append('<link href="css/style.css" rel="stylesheet">')
+	view.append('</head>\n')
+	view.append("<body>")
+
+		
+	view.append('<div class="container">')
+	view.append('<section class="panel panel-default clearfix">')
+	view.append('<div class="panel-heading">')
+	view.append('<h4>Pool Configuration</h4>')
+	view.append('</div>')
 	view.append("Welcome to the optipoolware public Bismuth pool")
 	view.append("<br>Config details in your miner.txt:")
 	view.append("<br>mining_ip=78.28.250.81")
 	view.append("<br>miner_address=your mining address")
 	view.append("<br>Download miner:https://github.com/maccaspacca/Optipoolware")
+	view.append('</div>')
+	view.append('</section>')
 
-	view.append('<head>\n')
-	view.append('<link rel = "icon" href = "static/explorer.ico" type = "image/x-icon" />')
-	view.append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >')
-	view.append('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script >')
-	view.append('<title>Bismuth Pool</title>')
-	view.append('</head>\n')
-
-	view.append('<body bgcolor = "white">\n')
-	view.append("<body>")
-
-	view.append("<h3>Share statistics</h3>")
-	view.append("<table class='table table-responsive'>")
-	view.append("<th>Address</th>")
-	view.append("<th>Number of shares</th>")
-	view.append("<th>Last hash kh/s</th>")
-	view.append("<th>Last worker</th>")
-	view.append("<th>First share date</th>")
+	view.append('<div class="container">')
+	view.append('<div id="node_alerts" class="alert alert-danger hidden"></div>')
+	view.append('<div class="alert alert-info hidden">2013-12-13: This is a message to miners on my node telling them something really cool.</div>')
+	view.append('<div id="header_content"></div>')
+	view.append('<section class="panel panel-default">')
+	view.append('<div class="panel-heading">')
+	view.append('<h4>Active Miners</h4>')
+	view.append('</div>')
+	view.append('<div class="table table-responsive">')
+	view.append('<table id="active_miners" class="table table-hover">')
+	view.append('<thead>')
+	view.append('<tr>')
+	view.append('<th class="text-left">Address</th>')
+	view.append('<th class="text-center">Number of shares</th>')
+	view.append('<th class="text-center">Est. total hashrate</th>')
+	view.append('<th class="text-center">Last worker</th>')
+	view.append('<th class="text-center">Workers</th>')
+	view.append('</tr>')
+	view.append('</thead>')
 
 	for x in addresses:
 		s.execute("SELECT sum(shares) FROM shares WHERE address = ? AND paid != 1", (x,))
@@ -70,18 +100,30 @@ def hello():
 
 		s.execute("SELECT * FROM shares WHERE address = ? ORDER BY timestamp DESC LIMIT 1", (x,))
 		shares_last = s.fetchone()
-		mrate = shares_last[4]
+		#mrate = shares_last[4]
 		mname = shares_last[5]
-
+		
+		s.execute("SELECT DISTINCT name FROM shares WHERE address = ?", (x,))
+		shares_names = s.fetchall()
+		wcount = len(shares_names)
+		
+		s.execute("SELECT avg(rate) FROM shares WHERE address = ?", (x,))
+		shares_hash = s.fetchone()[0]
+		if shares_hash == None:
+			shares_hash = 0
+			continue
+			
+		mrate = int(shares_hash) * int(wcount)
+		total_hash = total_hash + mrate
+		
 		color_cell = "white"
-
 
 		view.append("<tr bgcolor ={}>".format(color_cell))
 		view.append("<td>{}</td>".format(x))
-		view.append("<td>{}</td>".format(shares_sum))
-		view.append("<td>{}</td>".format(mrate))
-		view.append("<td>{}</td>".format(mname))
-		view.append("<td>{}</td>".format(time.strftime("%d/%m/%y at %H:%M:%S", time.gmtime(float(shares_timestamp)))))
+		view.append("<td class='text-center'>{}</td>".format(shares_sum))
+		view.append("<td class='text-center'>{} kh/s</td>".format(mrate))
+		view.append("<td class='text-center'>{}</td>".format(mname))
+		view.append("<td class='text-center'>{}</td>".format(str(wcount)))
 		view.append("<tr>")
 
 	try:
@@ -89,9 +131,16 @@ def hello():
 	except:
 		shares_total = 0
 
-
-
 	view.append("</table>")
+	view.append('</div>')
+	view.append('</div>')
+	view.append('</section>')
+
+	view.append('<div class="container">')
+	view.append('<section class="panel panel-default clearfix">')
+	view.append('<div class="panel-heading">')
+	view.append('<h4>Mined blocks for this round</h4>')
+	view.append('</div>')
 
 	try:
 		block_threshold = min(output_timestamps)
@@ -105,35 +154,54 @@ def hello():
 		view.append("<td>{}</td>".format(row[9]))
 		view.append("<tr>")
 		reward_list.append(float(row[9]))
+	
+	view.append('</div>')
+	
 
-	view.append("<th>Shares total</th>")
-	view.append("<td>{}</td>".format(shares_total))
+	view.append("<th width='50%'>Shares total</th>")
+	view.append("<td class='text-left'>{}</td>".format(shares_total))
 	view.append("<tr>")
 
 	reward_total = sum(reward_list)
 
-	view.append("<th>Reward per share</th>")
+	view.append("<th width='50%'>Reward per share</th>")
 	try:
 		reward_per_share = reward_total/shares_total
 	except:
 		reward_per_share = 0
 
-	view.append("<td>{}</td>".format(reward_per_share))
+	view.append("<td class='text-left'>{}</td>".format(reward_per_share))
 	view.append("<tr>")
 
-	view.append("<th>Mined rewards for this round</th>")
-	view.append("<td>{}</td>".format(reward_total))
+	view.append("<th width='50%'>Mined rewards for this round</th>")
+	view.append("<td class='text-left'>{}</td>".format(reward_total))
+	view.append("<tr>")
+	
+	view.append("<th width='50%'>Est pool hashrate (mh/s)</th>")
+	view.append("<td class='text-left'>{}</td>".format('%.2f' % (total_hash/1000)))
 	view.append("<tr>")
 
 	view.append("</table>")
+	view.append('</div>')
+	view.append('</section>')
 
+	
+	
 	# payout view
-	view.append("<h3>Pending payouts</h3>")
-	view.append("<table class='table table-responsive'>")
+	view.append('<div class="container">')
+	view.append('<section class="panel panel-default clearfix">')
+	view.append('<div class="panel-heading">')
+	view.append('<h4>Pending payouts</h4>')
+	view.append('</div>')
+	view.append('<div class="table table-responsive">')
+	view.append('<table id="active_miners" class="table table-hover">')
+	view.append('<thead>')
+	view.append('<tr>')
 	view.append("<th>Address</th>")
 	view.append("<th>Bismuth reward</th>")
+	view.append('</tr>')
+	view.append('</thead>')
 	view.append("<tr>")
-
 
 	for x, y in zip(addresses, output_shares):
 
@@ -147,28 +215,51 @@ def hello():
 		view.append("<tr>")
 	# payout view
 	view.append("</table>")
+	view.append('</div>')
+	view.append('</div>')
+	view.append('</section>')
+	
+	
+	view.append('<div class="container">')
+	view.append('<div id="node_alerts" class="alert alert-danger hidden"></div>')
+	view.append('<div class="alert alert-info hidden">2013-12-13: This is a message to miners on my node telling them something really cool.</div>')
+	view.append('<div id="header_content"></div>')
+	view.append('<section class="panel panel-default">')
+	view.append('<div class="panel-heading">')
+	view.append('<h4>Previous payouts</h4>')
+	view.append('</div>')
+	view.append('<div class="table table-responsive">')
+	view.append('<table id="active_miners" class="table table-hover">')
+	view.append('<thead>')
+	view.append('<tr>')
+	view.append('<th class="text-left">Address</th>')
+	view.append('<th class="text-right">Bismuth reward</th>')
+	view.append('<th class="text-right">Block height</th>')
+	view.append('<th class="text-right">Time</th>')
+	view.append('</tr>')
+	view.append('</thead>')
 
-	# history view
-	view.append("<table class='table table-responsive'>")
-	view.append("<h3>Previous payouts</h3>")
-	view.append("<th>Address</th>")
-	view.append("<th>Bismuth reward</th>")
-	view.append("<th>Block height</th>")
-	view.append("<th>Time</th>")
-	view.append("<tr>")
-	for row in c.execute("SELECT * FROM transactions WHERE address = ? and openfield = ? ORDER BY timestamp DESC LIMIT 20",(address,)+("pool",)):
+	o.execute("SELECT DISTINCT address FROM shares")
+	add_num = o.fetchall()
+	acount = str(len(add_num))
+	
+	for row in c.execute("SELECT * FROM transactions WHERE address = ? and openfield = ? ORDER BY timestamp DESC LIMIT ?",(address,)+("pool",)+(acount,)):
 		view.append("<td>{}</td>".format(row[3]))
-		view.append("<td>{}</td>".format(row[4]))
-		view.append("<td>{}</td>".format(row[0]))
-		view.append("<td>{}</td>".format(time.strftime("%Y/%m/%d,%H:%M:%S", time.gmtime(float(row[1])))))
+		view.append("<td class='text-right'>{}</td>".format(row[4]))
+		view.append("<td class='text-right'>{}</td>".format(row[0]))
+		view.append("<td class='text-right'>{}</td>".format(time.strftime("%Y/%m/%d,%H:%M:%S", time.gmtime(float(row[1])))))
 		view.append("<tr>")
 
-	# history view
+	view.append("</table>")
+	view.append('</div>')
+	view.append('</div>')
+	view.append('</section>')
 
 	view.append("</body>")
 
 	conn.close()
 	shares.close()
+	oldies.close()
 
 	return ''.join(view)
 
